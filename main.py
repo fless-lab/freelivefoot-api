a7093b4f084c4202025c517d70dd6be5cb7195b7
fless-lab <achilleatarmla@gmail.com>
2022-03-01 01:36:51 +0000

    Ajout des générateurs d'infos et changement de l'index

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 
 from fastapi import FastAPI,HTTPException
 import json
+import socket
+import sys
 from urllib.parse import urlparse
 from helper import fetchData, getIframeLink, getOnlyFoot, isValidUrl
 
 app = FastAPI()
+
+
+#Infos
+hostname = socket.gethostname()
+version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
 # with open("matches.json") as m:
 #     temp = json.load(m)
@@ -13,9 +20,16 @@ temp =  fetchData()
 data = getOnlyFoot(temp)
 #data_length = len(data)
 
-
 @app.get("/")
-def index(page:int=1,size:int=1000): #Apres je remet ça à 10 afin de proceder à la pagination coté mobile
+async def index():
+    return {
+        "name": "freelivefoot",
+        "host": hostname,
+        "version": f"Bonjour ! Avec FastAPI executant sur Uvicorn. Utilisant Python {version}"
+    }
+
+@app.get("/matches")
+def get_matches(page:int=1,size:int=1000): #Apres je remet ça à 10 afin de proceder à la pagination coté mobile
     start = (page-1)*size
     end = start+size
     return data[start:end]
