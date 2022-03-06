9c056385e56881825b3b038d0f395b3487e4edbb
fless-lab <achilleatarmla@gmail.com>
2022-03-06 23:51:31 +0000

    fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI,HTTPException
+from fastapi import Body, FastAPI,HTTPException
 import json
 import socket
 import sys
@@ -29,10 +29,10 @@ def index(page:int=1,size:int=1000): #Apres je remet ça à 10 afin de proceder 
 
 #Ajouter un post au niveau du mobile pour retrieve le lien seulement une seule fois que c'est selectionné;..Bien sur pour le premier lien , ça doit etre ça.
 @app.post("/live")
-def live_link(link:str):
+def live_link(link:str=Body(...)):
     url = getIframeLink(link)
     if isValidUrl(url) : 
-        return url
+        return {"success":True,"url":url}
     raise HTTPException(status_code=400,detail="Source non valide.")
 
 @app.get("/infos")
